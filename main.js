8b2430b3d227483675bbaef840a2288605b16932
DuckyXD165 <tymumm122@gmail.com>
2024-02-13 14:34:09 -0600

    add

--- main.js
+++ main.js
@@ -22,6 +22,25 @@ auth.signInWithEmailAndPassword(email, password)
   })
 });
 
+const signupBtn = document.querySelector('#signup-btn');
+    signupBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log('User signed up!');
+  });
+});
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.email + " is logged in!");
+    } else {
+      console.log('User is logged out!');
+    }
+  });
 
 const logoutBtn = document.querySelector('#logout-btn');
 logoutBtn.addEventListener('click', e => {
