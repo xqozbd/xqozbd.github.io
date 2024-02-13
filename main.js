cb2810213cd091f5e6242952d0e2d6ea1a5d87e7
DuckyXD165 <tymumm122@gmail.com>
2024-02-13 14:28:10 -0600

    Update

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ function toggleSidebar() {
     main.classList.toggle("active");
 }
 
+const loginBtn = document.querySelector('#login-btn');
+loginBtn.addEventListener('click', e => {
+e.preventDefault();
+
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+
+auth.signInWithEmailAndPassword(email, password)
+  .then(cred => {
+    console.log('Logged in user!');
+  })
+  .catch(error => {
+    console.log(error.message);
+  })
+});
+
+
+const logoutBtn = document.querySelector('#logout-btn');
+logoutBtn.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut();
+  console.log('User signed out!');
+})
